f9d1deffcf70943f1e54d007293a5bc4ab7e7851
PEP <paulo@paletta.com.br>
2022-03-16 01:43:56 -0300

    Beta 20221603b

--- index.py
+++ index.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-  
 
-import dash
+#import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 import dash_trich_components as dtc
 from dash.dependencies import Input, Output, State
 import pandas as pd
 
-app = dash.Dash(__name__, suppress_callback_exceptions=False, title='KWT-Community', external_stylesheets=[dbc.themes.GRID, dbc.icons.FONT_AWESOME])
-server = app.server   
+from app import app 
+
+#app = dash.Dash(__name__, suppress_callback_exceptions=False, title='KWT-Community', external_stylesheets=[dbc.themes.GRID, dbc.icons.FONT_AWESOME])
+#server = app.server   
 
 #theme_toggle = dtc.ThemeToggle()
 
