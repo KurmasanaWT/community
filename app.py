ee9932dcfdc9fa59120ae8124676de956f50023b
PEP <paulo@paletta.com.br>
2022-03-16 16:30:01 -0300

    Beta 20220316d

--- app.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import dash
-import dash_bootstrap_components as dbc
- 
-app = dash.Dash(__name__, suppress_callback_exceptions=False, title='KWT-Community', external_stylesheets=[dbc.themes.GRID, dbc.icons.FONT_AWESOME])
-#server = app.server 
--- index.py
+++ index.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-  
 
-#import dash
+import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 import dash_trich_components as dtc
 from dash.dependencies import Input, Output, State
 import pandas as pd
 
-from app import app 
+#from app import app 
 
-#app = dash.Dash(__name__, suppress_callback_exceptions=False, title='KWT-Community', external_stylesheets=[dbc.themes.GRID, dbc.icons.FONT_AWESOME])
+app = dash.Dash(__name__, suppress_callback_exceptions=False, title='KWT-Community', external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME])
 server = app.server   
 
 #theme_toggle = dtc.ThemeToggle()
@@ -19,48 +19,55 @@ navbar = html.Div(className='topnav',
         html.A( html.Img(src="static/logo.png", height="47"), href="../") 
     ]) 
 
+desc="Normalização de médias e a distância dos preços em relação à mesma para avaliar as bandas de cima e de baixo. &amp; &amp \n &#13; &#013; &lt;br&gt; \A Sponsor: <b>RAFI</b> (Twitter @RaphaFigueredo)."
+
 cards = dbc.Container(dbc.Row(
     [
+     
+        dbc.Card([
+            dbc.CardHeader([html.H6("REVERSÃO À MÉDIA"), dbc.Badge("16/03/2022")]),
+            dbc.CardImg(src='static/mean.jpg'),
+            dbc.CardBody([
 
-        dtc.Card(
-            className='content',
-            link='https://github.com/KurmasanaWT/community/ram_raphi',
-            image='static/mean.jpg',
-            title='Reversão à Média',
-            description="Normalização de médias e a distância dos preços em relação à mesma para avaliar as bandas de cima e de baixo. Veja também html.A('Investopedia',href='https://www.investopedia.com/terms/m/meanreversion.asp')",
-            badges=['Python', 'FinTwit', 'Raphi'],
-            git='https://github.com/KurmasanaWT/community',
-            dark=True,
-        ),
-      
+                dbc.Button(html.I(className="fas fa-play"), href='../meanreversion.py', target="new"),
+                dbc.Button(html.I(className="fab fa-github"), href='https://github.com/KurmasanaWT', target="new"),
+
+                html.P("Normalização de médias e a distância dos preços em relação à mesma para avaliar as bandas de cima e de baixo."),
+
+                html.P([
+                    "Sponsor: ", 
+                    html.A([" RAFI | ",html.I(className="fab fa-twitter"), " @RaphaFigueredo)"], href='https://twitter.com/RaphaFigueredo', target="new")
+                ]),
+
+                dbc.Badge("Médias"),
+                dbc.Badge("FinTwit"),
+                dbc.Badge("RAFI"),
+
+            ], style={'text-align':'center', 'justify-content':'center'}),
+        ], className="cardSize", ),
+     
       ### incluir mais cards aqui
 
-    ]), fluid=True)
+    ], className='content'), fluid=True)
 
 comm_cards = dbc.Container(dbc.Row(
     [
         dbc.Card([
             dbc.CardHeader(html.Img(src="static/logo.png", height="30"), style={'text-align':'center'}),
             dbc.CardBody([
-                "Bem vindo!",
-                html.Br(),
-                "Este é o ",
-                html.Span("Kurmasana Wealth Tech Community hub.", style={'color':'#0a0', 'font-weight':'bolder'}),
-                html.P(),
-                "Como forma de contribuir para o desenvolvimento do mercado de capitais no Brasil, decidimos colaborar mais ativamente com a comunidade, desenvolvendo e pubicando gratuitamente códigos em Python a partir de sugestões de especialistas em finanças e economia.",
-                html.P(),
-                "As primeiras sugestões já estão chegando e em breve vamos disponibilizá-las gratuitamente para todos!",
-                html.P(),
-                "Notícias e anúncios serão publicados nessa seção do site. Não deixe de visitá-la frequentemente.",
-                html.P(),
-                "Siga a KWT no ",
-                html.P(html.A([html.I(className="fab fa-twitter"), " Twitter"], href='https://twitter.com/KurmasanaWT', target="new")),
-                " ou no ",
-                html.P(html.A([html.I(className="fab fa-github")," GitHub"], href='https://github.com/KurmasanaWT', target="new")),
-                "Ficaremos muito felizes com suas observações e contribuições.",
-                html.P(),
-                "Um abraço, Nós!"
-            ], style={'text-align':'center', 'justify-content':'center'}),
+                html.P("Bem vindo!"),
+                html.P([
+                    html.Span("Este é o "),
+                    html.P("Kurmasana Wealth Tech Community hub.", style={'color':'#0a0', 'font-weight':'bolder'})
+                    ], style={'text-align':'center'}),
+                html.P("Como forma de contribuir para o desenvolvimento do mercado de capitais no Brasil, decidimos colaborar mais ativamente com a comunidade, desenvolvendo e pubicando gratuitamente códigos em Python a partir de sugestões de especialistas em finanças e economia."),
+                html.P("As primeiras sugestões já estão chegando e em breve vamos disponibilizá-las gratuitamente para todos!"),
+                html.P("Notícias e anúncios serão publicados nessa seção do site. Não deixe de visitá-la frequentemente."),
+                html.P(["Siga a KWT no ", html.A([html.I(className="fab fa-twitter"), " Twitter"], href='https://twitter.com/KurmasanaWT', target="new")," ou no ", html.A([html.I(className="fab fa-github")," GitHub"], href='https://github.com/KurmasanaWT', target="new")]),
+                html.P("Ficaremos muito felizes com suas observações e contribuições."),
+                html.P("Um abraço, Nós!", style={'text-align':'right'}),
+                dbc.Badge("16/03/2022")
+            ]),
         ], className="cardSize", ),
       
       ### incluir mais cards aqui
@@ -70,54 +77,77 @@ comm_cards = dbc.Container(dbc.Row(
 kwt_cards = dbc.Container(dbc.Row(
     [
         dbc.Card([
-            dbc.CardHeader(html.H5("Quem somos Nós???", style={'text-align':'center', 'color':'#0a0', 'font-weight':'bolder'})),
+            dbc.CardHeader(html.H6("QUEM SOMOS NÓS ???")),
             dbc.CardBody([
-                html.Img(src="static/logo.png", height="30"),
-                html.Br(),
-                html.P("Este é o ",),
-                html.Span("Kurmasana Wealth Tech Community hub.", style={'color':'#0a0', 'font-weight':'bolder'}),
-            ], style={'text-align':'center', 'justify-content':'center'}),
+                html.P(html.Img(src="static/logo_only.png", height="60"), style={'text-align':'center'}),
+                html.P("Nós somos parte do universo das Start-Ups, operando no espaço existente entre os experts do mercado de capitais e os geeks do eco-sistema de inovação."),
+                html.P("Somos uma WEALTHTECH."),
+                html.P("As palavras “riqueza” e “tecnologia” se juntaram para dar origem a uma nova geração de empresas de tecnologia financeira que criam soluções digitais para transformar o setor de investimentos e gestão de ativos."),
+                html.P([
+                    dbc.Badge("WealthTech"),
+                    dbc.Badge("Artificiall Intelligence"),
+                    dbc.Badge("Machine Learning"),
+                ], style={'text-align':'center'}),
+            ], style={}),
         ], className="cardSize", ),
 
+        dbc.Card([
+            dbc.CardHeader(html.H6("FUNÇÃO, CRENÇA E SONHO")),
+            dbc.CardBody([
+                html.P("Nossa FUNÇÃO: Intensificar a integração no espaço entre experts e geeks."),
+                html.P("Nossa CRENÇA: Total democratização do conhecimento e da informação."),
+                html.P("Nosso SONHO: Liberté, Égalité, Fraternité!"),
+            ], style={}),
+        ], className="cardSize", ),
+
+        dbc.Card([
+            dbc.CardHeader(html.H6("ANIMAIS DE PODER")),
+            dbc.CardBody([
+                html.P("Nosso ANIMAL DE PODER: A Tartaruga, afinal devagar se vai longe!"),
+                html.P("Nos ensinamentos nativos americanos, a tartaruga é o símbolo mais velho do planeta Terra. É a personificação da deusa da energia e a Mãe eterna na qual nossa vida evolui. A tartaruga é considerada para o xamã como sendo o chão ou piso da Mãe Terra. Ela, com sua lentidão e grossa carapaça, nos ensina como podemos nos proteger."),
+                html.P("Nossos Cachorros: Filomena (Genérico) e Apollo (Golden Retriever)."),
+            ], style={}),
+        ], className="cardSize", ),
+
+
          dbc.Card([
-            dbc.CardHeader(html.H5("Sânscrito", style={'text-align':'center', 'color':'#0a0', 'font-weight':'bolder'})),
+            dbc.CardHeader(html.H6("SÂNSCRITO")),
             dbc.CardBody([
                 html.P("Kúrmásana dêvanágari कूर्मासन IAST kūrmāsana."),
                 html.P("Em sânscrito kúrma é tartaruga. Também dá nome a uma nádí do corpo energético ou um sub-prana."),
                 html.Span("Kūrma vāyu - responsável pelo piscar dos olhos. "),
                 html.I(className="fas fa-grin-wink")
-            ], style={'text-align':'center', 'justify-content':'center'}),
+            ], style={}),
         ], className="cardSize", ),
 
         dbc.Card([
-            dbc.CardHeader(html.H5("Mitologia Hindu", style={'text-align':'center', 'color':'#0a0', 'font-weight':'bolder'})),
+            dbc.CardHeader(html.H6("MITOLOGIA HINDU")),
             dbc.CardBody([
                 html.P("Na mitologia hindu, a Tartaruga Mundial, chamada Kurma ou Kacchapa, sustenta quatro elefantes nas costas; eles, por sua vez, carregam nas costas o peso do mundo inteiro."),
-                html.P("A tartaruga é um dos dez avatares ou encarnações do deus Vishnu."),
                 html.P("Conta a lenda hindu, que Vishnu tomou a forma de uma tartaruga, Kurma, para salvar a humanidade em uma batalha entre Devas (seres não humanos poderosos, tidos como divindades) e os Asuras (inimigos dos deuses ou demônios).")
-            ], style={'text-align':'center', 'justify-content':'center'}),
+            ], style={}),
         ], className="cardSize", ),
       
         dbc.Card([
-            dbc.CardHeader(html.H5("China", style={'text-align':'center', 'color':'#0a0', 'font-weight':'bolder'})),
+            dbc.CardHeader(html.H6("CHINA")),
             dbc.CardBody([
                 html.P("Na China, a tartaruga era um dos quatro animais sagrados no confucionismo."),
                  html.P("Durante o período Han, estelas eram montadas em cima de tartarugas de pedra, mais tarde ligadas a Bixi, o filho de casco de tartaruga do Rei Dragão."),
-            ], style={'text-align':'center', 'justify-content':'center'}),
+            ], style={}),
         ], className="cardSize", ),
 
         dbc.Card([
-            dbc.CardHeader(html.H5("Roma Antiga", style={'text-align':'center', 'color':'#0a0', 'font-weight':'bolder'})),
+            dbc.CardHeader(html.H6("ROMA ANTIGA")),
             dbc.CardBody([
                 html.P("Na Roma Antiga o exército usava a formação testudo ('tartaruga') onde os soldados formavam uma parede de escudos para proteção."),
-            ], style={'text-align':'center', 'justify-content':'center'}),
+            ], style={}),
         ], className="cardSize", ),
 
         dbc.Card([
-            dbc.CardHeader(html.H5("Esopo", style={'text-align':'center', 'color':'#0a0', 'font-weight':'bolder'})),
+            dbc.CardHeader(html.H6("ESOPO")),
             dbc.CardBody([
                 html.P("Nas Fábulas de Esopo, 'A Tartaruga e a Lebre' conta como uma corrida desigual pode ser vencida pelo parceiro mais lento."),
-            ], style={'text-align':'center', 'justify-content':'center'}),
+            ], style={}),
         ], className="cardSize", ),
       
       ### incluir mais cards aqui
@@ -131,7 +161,7 @@ sidebar = dtc.SideBar(className='sidenav',
         dtc.SideBarItem(id='id_3', label="Sobre Nós", icon="fas fa-user-astronaut"),
         #dtc.SideBarItem(id='id_4', label="GitHub", icon="fab fa-github"),
         #dtc.SideBarItem(id='id_5', label="Twitter", icon="fab fa-twitter"),
-    ], bg_color="#000")
+    ])
 
 main_content = html.Div([
     sidebar,
@@ -196,4 +226,4 @@ def toggle_collapse(input1, input2, input3):#, input4, input5):
     #    ])
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
