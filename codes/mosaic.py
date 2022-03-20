80e11c0a8c1ef771a45861999560535b796270e0
PEP <paulo@paletta.com.br>
2022-03-20 15:50:57 -0300

    World News update

--- codes/mosaic.py
+++ codes/mosaic.py
@@ -4,50 +4,56 @@ from dash import html
 from app import app
 
 layout = dbc.Container(
-    children=[
-        
-        dbc.Card([
-            dbc.CardHeader("NBC NEWS (Estados Unidos)"),
-            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/3UaQIMGmoDU?&autoplay=1")]),
-            ], className="cardSize-vid"),
+    children=[ 
 
         dbc.Card([
             dbc.CardHeader("EURONEWS (União Européia)"),
-            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/sPgqEHsONK8?&autoplay=1")]),
+            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/sPgqEHsONK8?&autoplay=1&mute=1")]),
             ], className="cardSize-vid"),
 
         dbc.Card([
             dbc.CardHeader("SKY NEWS (Reino Unido)"),
-            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/9Auq9mYxFEE?&autoplay=1")]),
+            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/9Auq9mYxFEE?&autoplay=1&mute=1")]),
             ], className="cardSize-vid"),    
 
         dbc.Card([
             dbc.CardHeader("FRANCE 24 (França)"),
-            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/jNhh-OLzWlE?&autoplay=1")]),
+            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/jNhh-OLzWlE?&autoplay=1&mute=1")]),
             ], className="cardSize-vid"),
 
         dbc.Card([
             dbc.CardHeader("DEUTSCH WELLE (Alemanha)"),
-            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/V9KZGs1MtP4?&autoplay=1")]),
+            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/V9KZGs1MtP4?&autoplay=1&mute=1")]),
             ], className="cardSize-vid"),
 
         dbc.Card([
             dbc.CardHeader("RT (Rússia)"),
-            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://odysee.com/$/embed/RTlivestream/8c06ebe369b6ecf6ad383e4a32bfca34c0168d79?r=RfLjh5uDhbZHt8SDkQFdZyKTmCbSCpWH&autoplay=1")]),
+            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://odysee.com/$/embed/RTlivestream/8c06ebe369b6ecf6ad383e4a32bfca34c0168d79?r=RfLjh5uDhbZHt8SDkQFdZyKTmCbSCpWH&autoplay=1&mute=1")]),
             ], className="cardSize-vid"),
  
-        dbc.Card([
-            dbc.CardHeader("CGTN EUROPE (China)"),
-            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/9Y-LKliWJ4U?&autoplay=1")]),
+         dbc.Card([
+            dbc.CardHeader("TRT WORLD (Turquia)"),
+            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/CV5Fooi8YJA?&autoplay=1&mute=1")]),
             ], className="cardSize-vid"),
 
         dbc.Card([
             dbc.CardHeader("AL JAZEERA (Catar)"),
-            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/-upyPouRrB8?&autoplay=1")]),
+            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/-upyPouRrB8?&autoplay=1&mute=1")]),
             ], className="cardSize-vid"),
   
+        dbc.Card([
+            dbc.CardHeader("CGTN EUROPE (China)"),
+            dbc.CardBody([html.Iframe(className="ytvid", width="420", height="315", src="https://www.youtube.com/embed/9Y-LKliWJ4U?&autoplay=1&mute=1")]),
+            ], className="cardSize-vid"),     
 
-        html.Div(dbc.Badge("* Todos os vídeos são transmitidos pelo YouTube, exceto o canal RT, transmitido pelo Odysee."))
+        html.Div(dbc.Badge(children=[
+            html.Span("* Todos os vídeos são transmitidos pelo "),
+            html.A(href='http://www.youtube.com', target='new', children='YouTube'),
+            html.Span(" exceto o canal RT, transmitido pelo "),
+            html.A(href='http://www.odysee.com', target='new', children='Odysee'),
+            html.Span(" .")
+        ], className="badge-link",
+        ))
 
     ], fluid=True
 )
